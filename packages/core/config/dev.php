657af17abe16a4f5c874fbf49985965ad81b7a3a
Maurice Preuß <envoyr@froxlor.org>
2026-06-18 16:13:29 +0200

    update environment variables in dev.php configuration

--- packages/core/config/dev.php
+++ packages/core/config/dev.php
@@ -3,10 +3,10 @@
 return [
     'node' => env('DEV_NODE', 'local'),
     'repositories' => env('DEV_REPOSITORIES', 'framework'),
-    'packages' => env('DEV_PACKAGES', ''),
+    'packages' => env('DEV_PACKAGES'),
     'first_name' => env('DEV_FIRST_NAME', 'froxlor'),
     'last_name' => env('DEV_LAST_NAME', 'Super-Admin'),
-    'email' => env('DEV_EMAIL', 'dev@froxlor.org'),
-    'password' => env('DEV_PASSWORD', 'DtQOWsmW9eH3rrlA9uujhDmY'),
-    'seed_development_data' => env('FROXLOR_SEED_DEVELOPMENT_DATA', false),
+    'email' => env('DEV_EMAIL'),
+    'password' => env('DEV_PASSWORD'),
+    'seed_development_data' => env('DEV_SEED_DEVELOPMENT_DATA', false),
 ];
